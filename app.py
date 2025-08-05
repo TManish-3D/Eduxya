315cc745251d722c2d5af3f4dee5a98056443797
TManish-3D <mtimsina91@gmail.com>
2025-08-05 12:47:41 +0545

    form

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash,redirect,url_for
+from form import Registration
 app=Flask(__name__)
+app.secret_key="loophole"
 @app.route('/')
 def home():
    return render_template("index.html")
+@app.route('/register',methods=["POST","GET"])
+def Register():
+   form=Registration()
+   if form.validate_on_submit():
+     name=form.name.data
+     email=form.email.data
+     flash(f"Welcome! {name}")
+     return render_template("success.html")
+   return render_template("register.html",form=form)
+@app.route('/success')
+def success():
+  return render_template('success.html')
+   
+   
 @app.route('/Entrance',methods=["POST","GET"])
 def Entrance():
    if request.method=="POST":
@@ -15,7 +31,11 @@ def Entrance():
 def feedback():
    if request.method=="POST":
     message=request.form.get("message")
-    return render_template("index.html")
+    if not  message:
+      flash("Empty input")
+      return redirect(url_for("feedback"))
+    flash("thank you")
+    return "thank you"
    return render_template('feedback.html')
      
      
